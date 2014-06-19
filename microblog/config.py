058cb091e974d7eabb382b76097a55a85b6a214f
whypro <whypro@live.cn>
2014-06-20 00:40:55 +0800

    优化配置文件的结构。

--- microblog/config.py
+++ microblog/config.py
@@ -3,10 +3,10 @@ from __future__ import unicode_literals
 import os
 import platform
 
+__all__ = ['LocalConfig', 'LocalDevelopmentConfig', 'BAEConfig', 'LocalDevelopmentBCSConfig']
 
 class Config(object):
     SECRET_KEY = 'I love you.'
-
     MAX_CONTENT_LENGTH = 4 * 1024 *1024   # 上传文件大小：4MB
     PER_PAGE = 10
     IMAGE_EXT = tuple('jpg jpe jpeg png gif svg bmp'.split())
@@ -15,10 +15,8 @@ class Config(object):
     # FLASK-THEMES
     DEFAULT_THEME = 'newstyle'
 
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
+    
+class LocalConfig(Config):
     # 本地服务器配置
     # 数据库配置
     DB_HOST = 'localhost'
@@ -26,28 +24,20 @@ class DevelopmentConfig(Config):
     DB_USERNAME = 'root'
     DB_PASSWORD = 'whypro'
     DB_PORT = int(3306)
-
     # FLASK-SQLALCHEMY
-    SQLALCHEMY_DATABASE_URI = \
-        'mysql://{username}:{password}@{host}:{port}/{database}'.format(
-            username=DB_USERNAME, password=DB_PASSWORD,
-            host=DB_HOST, port=DB_PORT,
-            database=DB_DATABASE
-        )
-
+    SQLALCHEMY_DATABASE_URI = 'mysql://{username}:{password}@{host}:{port}/{database}'.format(
+        username=DB_USERNAME, password=DB_PASSWORD,
+        host=DB_HOST, port=DB_PORT,
+        database=DB_DATABASE
+    )
+    
     TEMP_DIR = 'temp'
     UPLOADS_DIR = 'uploads'
 
 
-class DevelopmentBCSConfig(DevelopmentConfig):
-    API_KEY = 'IBWv1TZbjHGeC3euOCWCKQBE'
-    SECRET_KEY = '0QuvWjERsuDrwf6NntSe962SWPgGGj3o'
-    # BAE BCS 配置
-    USE_BCS_BUCKET = True
-    BCS_ADDR = 'bcs.duapp.com'
-    BCS_ACCESS_KEY = API_KEY
-    BCS_SECRET_KEY = SECRET_KEY
-    BCS_BUCKET_NAME = 'vieboo'
+
+class LocalDevelopmentConfig(LocalConfig):
+    DEBUG = True
 
 
 class BAEConfig(Config):
@@ -61,22 +51,28 @@ class BAEConfig(Config):
     DB_USERNAME = API_KEY
     DB_PASSWORD = SECRET_KEY
     DB_PORT = int(4050)
-    # TEMP_DIR = const.APP_TMPDIR
-
-    # BAE BCS 配置
-    BCS_ADDR = 'bcs.duapp.com'
-    BCS_ACCESS_KEY = API_KEY
-    BCS_SECRET_KEY = SECRET_KEY
-    BCS_BUCKET_NAME = 'vieboo'
-
-    USE_BCS_BUCKET = True
-
     # FLASK-SQLALCHEMY
     SQLALCHEMY_DATABASE_URI = 'mysql://{username}:{password}@{host}:{port}/{database}'.format(
         username=DB_USERNAME, password=DB_PASSWORD,
         host=DB_HOST, port=DB_PORT,
         database=DB_DATABASE
     )
+    # TEMP_DIR = const.APP_TMPDIR
+    USE_BCS_BUCKET = True
+    # BAE BCS 配置
+    BCS_ADDR = 'bcs.duapp.com'
+    BCS_ACCESS_KEY = API_KEY
+    BCS_SECRET_KEY = SECRET_KEY
+    BCS_BUCKET_NAME = 'vieboo'
+      
+    
+class LocalDevelopmentBCSConfig(LocalDevelopmentConfig, BAEConfig):
+    pass
+
+
+
+
+    
 
 
 
--- runserver.py
+++ runserver.py
@@ -7,11 +7,10 @@ sys.path.insert(0, deps_path)
 
 from microblog import create_app
 
-app = create_app('microblog.config.DevelopmentBCSConfig')
+app = create_app('microblog.config.LocalDevelopmentConfig')
 
 if 'SERVER_SOFTWARE' in os.environ:
     from bae.core.wsgi import WSGIApplication
     application = WSGIApplication(app)
 elif __name__ == '__main__':
-    print app.config['USE_BCS_BUCKET']
     app.run()
