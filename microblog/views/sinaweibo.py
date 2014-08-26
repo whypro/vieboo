0a4ecdbe13b449cff6d2fb75b690e456fbb18349
whypro <whypro@whypro-Laptop.WHYPRO-STUDIO>
2014-08-27 00:15:34 +0800

    增加 sinaweibo 分页功能

--- microblog/views/sinaweibo.py
+++ microblog/views/sinaweibo.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from flask import Blueprint
+from __future__ import unicode_literals, division
+from flask import Blueprint, abort
 from microblog.helpers import render_template, get_mongodb
-
+from microblog.models import Status
 
 sinaweibo = Blueprint('sinaweibo', __name__, url_prefix='/sinaweibo')
 
 
 
-sinaweibo.route('/')
-def index():
-    return 'hehe'
+@sinaweibo.route('/', defaults={'page': 1})
+@sinaweibo.route('/page/<int:page>/')
+def index(page):
     connection = get_mongodb()
-    statuses = connection.Status.find().sort([('reposts_count', -1)])
-    return render_template('mongodb/index.html', statuses=statuses)
+    connection.register([Status])
 
+    page_count = 10
+    start = page_count * (page - 1)
+    max_page = connection.Status.find().count() // page_count
+    if page < 1 or page > max_page:
+        abort(404)
 
+    statuses = connection.Status.find().sort([('reposts_count', -1)]).skip(start).limit(page_count)
+    return render_template('sinaweibo/index.html', statuses=statuses, page=page, max_page=max_page)
