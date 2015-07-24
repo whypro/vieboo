410884740354afa3a1dd07826f16997c61e5848c
whypro <whypro@live.cn>
2015-07-24 18:34:53 +0800

    Fix bugs of db config

--- microblog/extensions.py
+++ microblog/extensions.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
 from flask.ext.sqlalchemy import SQLAlchemy
 # from flask.ext.uploads import UploadSet, IMAGES
-
-
-class nullpool_SQLAlchemy(SQLAlchemy):
-    def apply_driver_hacks(self, app, info, options):
-        super(nullpool_SQLAlchemy, self).apply_driver_hacks(app, info, options)
-        from sqlalchemy.pool import NullPool
-        options['poolclass'] = NullPool
-        del options['pool_size']
-
-# db = SQLAlchemy()
-db = nullpool_SQLAlchemy()
+db = SQLAlchemy()
 # photos = UploadSet('photos', IMAGES)
