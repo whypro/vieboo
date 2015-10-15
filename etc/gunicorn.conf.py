eb4d5dbd8fef0bc82a21ecbca0287109ef42f64e
whypro <whypro@live.cn>
2015-10-15 21:10:51 +0800

    Add gunicorn & supervisor config file

---
+++ etc/gunicorn.conf.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+import gevent.monkey
+gevent.monkey.patch_all()
+import multiprocessing
+
+#bind = 'unix:/var/run/ibati.sock'
+bind = '127.0.0.1:10042'
+max_requests = 20
+keepalive = 5
+
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'gunicorn.workers.ggevent.GeventWorker'
+
+loglevel = 'info'
+errorlog = '-'
+
+x_forwarded_for_header = 'X-FORWARDED-FOR'
+
+secure_scheme_headers = {
+    'X-SCHEME': 'https',
+}
